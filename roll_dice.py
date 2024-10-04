6b7389a6341639b89df14f83a775af39ceecd2b5
RoqueJosue <josue.roque@fwcsstudents.org>
2024-10-04 13:08:56 -0400

    Add files via upload

--- roll_dice.py
+++ roll_dice.py
@@ -1,6 +1,42 @@
 import random
 
-def rolls():
+def roll_1():
+    dice1 = random.randint(1,6)
+    print(dice1, end=" ")
+
+
+def roll_2():
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    print(dice1, end=" ")
+    print(dice2, end=" ")
+
+
+
+
+def roll_3():
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice3 = random.randint(1,6)
+    print(dice1, end=" ")
+    print(dice2, end=" ")
+    print(dice3, end=" ")
+
+
+
+
+def roll_4():
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice3 = random.randint(1,6)
+    dice4 = random.randint(1,6)
+    print(dice1, end=" ")
+    print(dice2, end=" ")
+    print(dice3, end=" ")
+    print(dice4, end=" ")
+
+
+def roll_5():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice3 = random.randint(1,6)
@@ -12,4 +48,3 @@ def rolls():
     print(dice4, end=" ")
     print(dice5, end=" ")
 
-rolls()
