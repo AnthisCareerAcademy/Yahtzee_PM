c4b8290c693ee1b9a905b3e8a5d65d7305543418
RoqueJosue <josue.roque@fwcsstudents.org>
2024-10-04 13:00:33 -0400

    Add files via upload

---
+++ roll_dice.py
@@ -0,0 +1,15 @@
+import random
+
+def rolls():
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice3 = random.randint(1,6)
+    dice4 = random.randint(1,6)
+    dice5 = random.randint(1,6)
+    print(dice1, end=" ")
+    print(dice2, end=" ")
+    print(dice3, end=" ")
+    print(dice4, end=" ")
+    print(dice5, end=" ")
+
+rolls()
