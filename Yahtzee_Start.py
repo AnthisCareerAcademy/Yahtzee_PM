adc76fde28a6cb4317aa2cb56083983360db70ea
Nicolecw <nicole.wilson.2@fwcsstudents.org>
2024-10-25 13:23:01 -0400

    Update Yahtzee_Start.py

--- Yahtzee_Start.py
+++ Yahtzee_Start.py
@@ -56,8 +56,9 @@ print('\n')
 
 response = input('Would You Like to Read The Rules? (yes/no) ')
 if response == 'yes':
-    print('Each player takes a score card. To decide who goes first, each player in turn rolls all 5 dice.'
-          ' The player with the highest total goes first. Play then passes to the left.')
+    rules = ('Each player takes a score card. To decide who goes first, each player in turn rolls all 5 dice. '
+             'The player with the highest total goes first.') # Expand
+    print(rules)
 else:
     print('Starting Game')
 
