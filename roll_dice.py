e75a5a1d468af7e8a2b2add4ecc534a077a55dfc
Caleb York <caleb.york@fwcsstudents.org>
2024-10-25 13:18:52 -0400

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -1,15 +1,28 @@
 import random
 
-def rolls():
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    dice3 = random.randint(1,6)
-    dice4 = random.randint(1,6)
-    dice5 = random.randint(1,6)
-    print(dice1, end=" ")
-    print(dice2, end=" ")
-    print(dice3, end=" ")
-    print(dice4, end=" ")
-    print(dice5, end=" ")
 
-rolls()
+
+def roll_1():
+    dice1 = random.randint(1,6)
+    return dice1
+
+
+def roll_2():
+    dice2 = random.randint(1,6)
+    return dice2
+
+
+def roll_3():
+    dice3 = random.randint(1,6)
+    return dice3
+
+
+def roll_4():
+    dice4 = random.randint(1,6)
+    return dice4
+
+
+def roll_5():
+    dice5 = random.randint(1,6)
+    return dice5
+
